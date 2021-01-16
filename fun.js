74f9dc51b192af95a4cef9180fb936dcb4a68fb8
Luis Vasquez <luisvbdr0301@gmail.com>
2021-01-16 00:25:09 -0430

    nav fixed two

--- fun.js
+++ fun.js
@@ -1,7 +1,9 @@
 const close = document.getElementById("close");
 const nav = document.getElementById("ham");
+const navstiky = document.getElementById("navstiky");
 
 close.addEventListener("click",() => { 
     nav.classList.toggle("act");
+    navstiky.classList.toggle("actnav");
     close.classList.toggle("toggle");
 });
